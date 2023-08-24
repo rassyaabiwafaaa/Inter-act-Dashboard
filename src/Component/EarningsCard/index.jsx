26e9980a603f730ce71e3c42fab9a27d89b6593f
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-08-24 18:16:16 +0700

    install react icons and create desc for earnings card

--- src/Component/EarningsCard/index.jsx
+++ src/Component/EarningsCard/index.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BsChevronCompactDown } from "react-icons/bs";
 
 export default function EarningsCard() {
-  return <div className="w-[285px] h-[220px] rounded-[5px] bg-24"></div>;
+  return (
+    <div className="w-[285px] h-[220px] rounded-[5px] bg-24">
+      <div className="mx-[20px]">
+        {/* Text */}
+        <div className="flex justify-between items-center pt-[20px]">
+          <span className="font-[700] text-[15px] text-white">Earnings</span>
+          <span className="font-[400] text-[10px] text-[#5c6ca5]">
+            <span className="flex items-center gap-2">
+              This Week <BsChevronCompactDown className="text-[9px] cursor-pointer" />
+            </span>
+          </span>
+        </div>
+        {/* Chart and desc */}
+      </div>
+    </div>
+  );
 }
--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -3,7 +3,7 @@ import { ProfilePicture } from "../../assets/images";
 
 export default function Header() {
   return (
-    <div className="h-[50px] w-full py-[15px] bg-26 border-[#4b5259] border-b-[1px] flex justify-end">
+    <div className="h-[50px] w-full py-[30px] bg-26 border-[#4b5259] border-b-[1px] flex justify-end">
       {/* User profile */}
       <div className="flex items-center mr-[44.5px]">
         <img src={ProfilePicture} alt="profile-picture" />
