d4cb0aab69a226bbb9ecd5acc93f83b89bceeb65
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-08-24 17:54:02 +0700

    add active event click sidebar menu

--- src/Component/Sidebar/index.jsx
+++ src/Component/Sidebar/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logo } from "../../assets/images";
-import { HomeIcon } from "../../assets/icons";
 import { sideBarMenu } from "../../const";
 
 export default function SideBar() {
+  const [active, setActive] = useState("Home");
+
+  const handleClickSideBarMenu = (e) => {
+    setActive(e.target.id);
+  };
   return (
     <div className="w-[250px] bg-100 h-[100vh] fixed">
       <div className="pt-[20px] pl-[20px]">
@@ -16,11 +20,11 @@ export default function SideBar() {
         <div className="mt-[62px]">
           <ul className="flex flex-col gap-[8px]">
             {sideBarMenu.map((item) => (
-              <li className="h-[48px] bg-18 w-[210px] rounded-[8px] flex items-center pl-[14px]">
-                <a href="#" className="text-49 font-medium flex items-center gap-[12px]">
+              <li key={item.text} id={item.text} className={`h-[48px] ${active === item.text && "bg-18"} w-[210px] rounded-[8px] flex items-center pl-[14px] cursor-pointer`} onClick={handleClickSideBarMenu}>
+                <div href="#" className="text-49 font-medium flex items-center gap-[12px]">
                   <img src={item.icons} alt="Home icon" className="" />
                   {item.text}
-                </a>
+                </div>
               </li>
             ))}
           </ul>
