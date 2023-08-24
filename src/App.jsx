ce1cf4b59ff40fcb95264f3ad74c194bb9c6ee6d
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-08-24 14:06:23 +0700

    create layout for dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,12 @@ function App() {
         {/* Header */}
         <Header />
         {/* Dashboard Content */}
-        <div className="mt-[45px] pl-[32px] pr-[32px]">
+        <div className="mt-[45px] ml-[250px] pl-[32px] pr-[32px] pb-3">
           {/* Title page */}
           <TitlePage />
 
           {/* Content */}
-          <div className="w-full h-[78.6vh] mt-[32px] bg-green-700">
+          <div className="w-full h-full mt-[32px] bg-green-700">
             {/* Summary */}
             <div className="h-[100px] bg-gray-200 flex flex-row justify-between  gap-[25px]">
               <SummaryCard />
@@ -27,13 +27,22 @@ function App() {
             </div>
 
             {/* charts */}
-            <div>
+            <div className="mt-[25px] flex justify-between">
               {/* Left Content */}
+              <div className="w-[663px] h-full bg-gray-600">
+                {/* Top left Chart */}
+                <div className="h-[324px] w-full bg-orange-300">top left chart</div>
+                {/* Bottom Right Chart */}
+                <div className="h-[362px] w-full bg-orange-400 mt-[18px]">bottom left chart</div>
+              </div>
+
               {/* Right Content */}
+              <div className="w-[436px] h-[602] bg-gray-600"></div>
             </div>
           </div>
 
           {/* Footer */}
+          <div className="bg-white h-[20px] mt-[36px]"></div>
         </div>
       </div>
     </div>
--- src/Component/Sidebar/index.jsx
+++ src/Component/Sidebar/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SideBar() {
   return (
-    <div className="w-[250px] bg-gray-800 h-[100vh]">
+    <div className="w-[250px] bg-gray-800 h-[100vh] fixed">
       <div className="pt-[20px] pl-[20px]">
         {/* Logo */}
         <div className="w-[132px] h-[35px] bg-orange-400"></div>
