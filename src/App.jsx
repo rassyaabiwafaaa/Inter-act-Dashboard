2e2f813ffbe7bbffce1edd21d2e173d96e654137
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-08-24 10:39:40 +0700

    add title page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./Component/Header";
 import SideBar from "./Component/Sidebar";
+import TitlePage from "./Component/TitlePage";
 
 function App() {
   return (
     <div className="flex">
       <SideBar />
-      <Header />
+      <div className="bg-red-500 w-full">
+        <Header />
+        <div className="mt-[45px]">
+          <TitlePage />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Component/TitlePage/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TitlePage() {
+  return <div className="h-[63px] bg-gray-500 w-full"></div>;
+}
