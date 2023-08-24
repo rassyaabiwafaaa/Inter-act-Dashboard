ee369b2f5ea403d7af09dee0be99349016a64152
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-08-24 16:45:02 +0700

    create title page

--- src/Component/TitlePage/index.jsx
+++ src/Component/TitlePage/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function TitlePage() {
-  return <div className="h-[63px] bg-gray-500 w-full">Title Page</div>;
+  return (
+    <div className="h-[63px] w-full">
+      <div>
+        <h1 className="font-[600] text-[30px] text-white">Marketing Dashboard</h1>
+        <span className="font-[500] text-[14px] text-49">
+          Home / <span className="text-22">Dashboard</span>
+        </span>
+      </div>
+    </div>
+  );
 }
