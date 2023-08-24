d04d395cdb09ac4e35e954b5b0d5403167ca07b0
Rassya Abiwafa Chairulditya <rassya.abiwafa@gmail.com>
2023-08-24 19:39:42 +0700

    create profit card text content

--- src/Component/ProfitCard/index.jsx
+++ src/Component/ProfitCard/index.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function ProfitCard() {
-  return <div className="w-[162px] h-[220px] bg-24 rounded-[5px]"></div>;
+  return (
+    <div className="w-[162px] h-[220px] bg-24 rounded-[5px]">
+      <div className="p-[20px] flex flex-col gap-[6.39px]">
+        <span className="text-white font-[500] text-[16.6px]">Profit</span>
+        <span className="text-22 font-[500] text-[28.46px]">8,24k</span>
+      </div>
+    </div>
+  );
 }
